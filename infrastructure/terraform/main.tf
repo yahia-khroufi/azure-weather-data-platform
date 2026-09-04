5d53a596507b3a23e1a9019cafa9c12c61245fc4
yahia-khroufi <yahiakhroufi@gmail.com>
2026-09-05 00:10:13 +0100

    add sql server and sql datqbase

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -4,6 +4,8 @@ locals {
   data_factory_name         = "adf-weather-${var.environment}-${var.unique_suffix}"
   databricks_workspace_name = "dbw-weather-${var.environment}-${var.unique_suffix}"
   databricks_connector_name = "dbc-weather-${var.environment}-${var.unique_suffix}"
+  sql_server_name           = "sql-weather-${var.environment}-${var.unique_suffix}"
+  sql_database_name         = "sqldb-weather-${var.environment}"
 }
 
 resource "azurerm_resource_group" "weather" {
@@ -122,3 +124,36 @@ resource "azurerm_role_assignment" "adf_databricks_contributor" {
   principal_id         = azurerm_data_factory.weather.identity[0].principal_id
 }
 
+resource "azurerm_mssql_server" "weather" {
+  name                          = local.sql_server_name
+  resource_group_name           = azurerm_resource_group.weather.name
+  location                      = azurerm_resource_group.weather.location
+  version                       = "12.0"
+  minimum_tls_version           = "1.2"
+  public_network_access_enabled = true
+
+  azuread_administrator {
+    login_username              = "Terraform Entra Administrator"
+    object_id                   = data.azurerm_client_config.current.object_id
+    tenant_id                   = data.azurerm_client_config.current.tenant_id
+    azuread_authentication_only = true
+  }
+}
+
+resource "azurerm_mssql_database" "weather" {
+  name                        = local.sql_database_name
+  server_id                   = azurerm_mssql_server.weather.id
+  sku_name                    = "GP_S_Gen5_1"
+  max_size_gb                 = 32
+  min_capacity                = 0.5
+  auto_pause_delay_in_minutes = 60
+  storage_account_type        = "Local"
+  zone_redundant              = false
+}
+
+resource "azurerm_mssql_firewall_rule" "allow_azure_services" {
+  name             = "AllowAzureServices"
+  server_id        = azurerm_mssql_server.weather.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
